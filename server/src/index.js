446495432f44e986e40ec99953cf3cef9f3a589c
Eduardo Barreira <edubarreira1212@gmail.com>
2024-06-13 10:45:59 -0300

    feat: using cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors"
 import { getRedisInstance } from "./redis.js";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors())
 
 app.post("/api/update-notepad", (req, res) => {
     const {noteName, noteContent} = req.body;
