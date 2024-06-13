e27c7ecdcb889298a619b8f55e9c968075eae8a8
Eduardo Barreira <edubarreira1212@gmail.com>
2024-06-13 10:10:23 -0300

    feat: function to post data created

--- web/js/server.js
+++ web/js/server.js
@@ -4,6 +4,16 @@ const serverTextArea = document.getElementById("server-text-area");
 const searchParams = new URLSearchParams(window.location.search);
 serverName.textContent = searchParams.get("name");
 
-serverTextArea.addEventListener("keyup", (event) => {
+serverTextArea.addEventListener("keyup", async (event) => {
     console.log("Change");
+
+    const { value } = event.target;
+
+    await fetch(`http://localhost:3000/api/update-notepad`, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({noteName: searchParams.get("name"), noteContent: value})
+    })
 })
