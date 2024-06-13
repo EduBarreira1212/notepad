aac087065744ed861556a70dc4dbd01fc2eb702a
Eduardo Barreira <edubarreira1212@gmail.com>
2024-06-13 09:49:07 -0300

    feat: redis configuration file

---
+++ server/src/redis.js
@@ -0,0 +1,15 @@
+import redis, { Redis } from "ioredis";
+
+export const getRedisInstance = () => {
+    const instance = new Redis({
+        host: "redis-15251.c276.us-east-1-2.ec2.redns.redis-cloud.com",
+        port: "15251",
+        username: "default",
+        password: process.env.DB_PASSWORD
+    });
+
+    instance.on('connect', () => console.log("Redis connected"));
+    instance.on("error", (error) => console.log("Redis error", error));
+
+    return instance;
+}
