a2d4315892e837a82da86b8ffeb6b2b99d7b88f4
Eduardo Barreira <edubarreira1212@gmail.com>
2024-06-13 09:49:27 -0300

    feat: post method created

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,24 @@
 import express from "express";
+import { getRedisInstance } from "./redis";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.post("/api/update-notepad", (req, res) => {
+    const {noteName, noteContent} = req.body;
+
+    const redisInstance = getRedisInstance();
+
+    const noteObj = {
+        content: noteContent,
+    }
+
+    const expire = 60000 * 60 * 24;
+
+    redisInstance.set(noteName, JSON.stringify(noteObj), "PX", expire);
+})
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
