5cf6166ecc268e4d1293050c22c0795620249bca
Eduardo Barreira <edubarreira1212@gmail.com>
2024-06-13 10:17:09 -0300

    feat: function to get data when window load

--- web/js/server.js
+++ web/js/server.js
@@ -2,7 +2,8 @@ const serverName = document.getElementById("server-name");
 const serverTextArea = document.getElementById("server-text-area");
 
 const searchParams = new URLSearchParams(window.location.search);
-serverName.textContent = searchParams.get("name");
+const serverNameURL = searchParams.get("name");
+serverName.textContent = serverNameURL
 
 serverTextArea.addEventListener("keyup", async (event) => {
     console.log("Change");
@@ -14,6 +15,14 @@ serverTextArea.addEventListener("keyup", async (event) => {
         headers: {
             "content-type": "application/json"
         },
-        body: JSON.stringify({noteName: searchParams.get("name"), noteContent: value})
+        body: JSON.stringify({noteName: serverNameURL, noteContent: value})
     })
+})
+
+window.addEventListener("load", async () => {
+    const data = await fetch(`http://localhost:3000/api/get-notepad/${serverNameURL}`).then(res => res.json());
+
+    serverTextArea.value = data.content;
+
+    console.log(data);
 })
