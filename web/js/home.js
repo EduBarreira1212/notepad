304ba1d4b1c8a7ae42bd2e2df38efcc46e31220f
Eduardo Barreira <edubarreira1212@gmail.com>
2024-06-20 13:41:33 -0300

    feat: setting local storage

--- web/js/home.js
+++ web/js/home.js
@@ -1,9 +1,16 @@
 const input = document.getElementById("server-input");
 const button = document.getElementById("server-input-btn");
+const userInput = document.getElementById("user-input");
 
 button.addEventListener("click", () => {
-    if(!input.value){
+    if(!input.value && !userInput.value){
         return;
     }
+
+    localStorage.setItem("username", userInput.value);
+
+    const randomNumber = Math.floor(Math.random() * 1000);
+    localStorage.setItem("user_id", `${userInput.value}-${randomNumber}`);
+
     window.location.href = `./server.html?name=${input.value}`
 })
