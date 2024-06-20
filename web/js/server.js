1a93a27a46868a11d11e0fffcb7a2508fb061b3b
Eduardo Barreira <edubarreira1212@gmail.com>
2024-06-20 15:09:52 -0300

    feat: adding authentication method

--- web/js/server.js
+++ web/js/server.js
@@ -1,19 +1,34 @@
 const serverName = document.getElementById("server-name");
+const userQty = document.getElementById("users-quantity");
 const serverTextArea = document.getElementById("server-text-area");
 
 const searchParams = new URLSearchParams(window.location.search);
 const serverNameURL = searchParams.get("name");
 serverName.textContent = serverNameURL
 
-Pusher.logToConsole = true;
-
 const pusher = new Pusher('7618bc1ca4744eea6825', {
-  cluster: 'us2'
+  cluster: 'us2',
+  channelAuthorization: {
+    endpoint: "http://localhost:3000/pusher/auth",
+    paramsProvider: () => {
+        return {
+            user_id: localStorage.getItem("user_id"),
+            username: localStorage.getItem("username"),
+        }
+    }
+  },
+  userAuthentication: {
+    endpoint: "http://localhost:3000/pusher/authenticate",
+    paramsProvider: () => {
+        return {
+            user_id: localStorage.getItem("user_id"),
+            username: localStorage.getItem("username"),
+        }
+    }
+  }
 });
 
 serverTextArea.addEventListener("keyup", async (event) => {
-    console.log("Change");
-
     const { value } = event.target;
 
     await fetch(`http://localhost:3000/api/update-notepad`, {
@@ -29,15 +44,27 @@ window.addEventListener("load", async () => {
     const data = await fetch(`http://localhost:3000/api/get-notepad/${serverNameURL}`).then(res => res.json());
 
     serverTextArea.value = data.content;
-
-    console.log(data);
 })
 
 if(serverNameURL){
+    pusher.signin();
+
     const channel = pusher.subscribe(serverNameURL);
     channel.bind("updated-note", data => {
         if (data.content && data.userId !== pusher.sessionID) {
             serverTextArea.value = data.content;
         }
     });
+
+    channel.bind("pusher:member_added", () => {
+        userQty.textContent = Number(userQty.textContent) + 1;
+    })
+
+    channel.bind("pusher:member_removed", () => {
+        userQty.textContent = Number(userQty.textContent) - 1;
+    })
+
+    channel.bind("pusher:subscription_succeeded", () => {
+        userQty.textContent = channel.members.count;
+    })
 }
